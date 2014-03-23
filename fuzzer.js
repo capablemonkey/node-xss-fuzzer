daee0987ec05c2142939dc2b67093ccc24693a83
Gordon Zheng <technix1@gmail.com>
2014-03-23 00:31:06 -0400

    added express

--- fuzzer.js
+++ fuzzer.js
@@ -1,5 +1,9 @@
 http = require("http")
 jsdom = require('jsdom')
+express = require('express')
+app = express()
+
+app.use(express.bodyParser())
 
 list_of_XSS_strings = ["foo", "bar"]
 
@@ -7,8 +11,8 @@ function exploitForm(form, callback){
 	// set all inputs to XSS strings
 	list_of_XSS_strings.forEach(function(evilstr) {
 		form.children.each(function(child){
-		child.value = evilstr
-	})
+			child.value = evilstr
+		})
 	})
 	
 	// submit the form, store response
@@ -26,19 +30,17 @@ function exploitForm(form, callback){
 
 
 
-function index(req, res) {
+app.post("/submit", function(req, res) {
 	function finished(evilstring, fieldname, formname) {
 		res.end("yay, we found it!", evilstring)
 	}
-
-	form = ""
 
 	// config = {}
 	// config.document = {}
 	// config.document.cookie = "GRUYERE=109040289|hello||author"
 
 	jsdom.env(
-	  "http://google-gruyere.appspot.com/664058118804/snippets.gtl",
+	  req.body['url'],
 	  ["http://code.jquery.com/jquery.js"],
 	  function (errors, window) {
 		
@@ -50,9 +52,12 @@ function index(req, res) {
 
 	  }
 	);
+});
 
-}
+app.get("/", function(req, res) {
+	res.end('<html><form action="/submit" method="POST">Target: <input name="url" /><button>go go go</button></form></html>')
+});
 
-server = http.createServer(index);
-
-server.listen(3000);
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
